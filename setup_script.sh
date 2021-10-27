9c2911c776d3a172a1c0f6da61a2858855fcc50b
Siddharth Sachdeva <siddsach@gmail.com>
2021-10-27 10:38:09 -0700

    Update setup script

--- setup_script.sh
+++ setup_script.sh
@@ -2,24 +2,24 @@
 sudo apt-get update
 
 # install utilities
-sudo apt-get -y install bzip2 wget git
+sudo apt-get -y install bzip2 wget git unzip
 
 # ANACONDA
 # download Anaconda linux version (link from page https://www.continuum.io/downloads)
 anaconda="Anaconda3-4.4.0-Linux-x86_64.sh" # update if necessary
-wget -P Downloads/ https://repo.continuum.io/archive/${anaconda}
+wget -P ~/Downloads/ https://repo.continuum.io/archive/${anaconda}
 # install anaconda - accept default options except yes to prepend anaconda path to PATH
 bash ~/Downloads/${anaconda}
 
 # MINICONDA
 # download Miniconda linux version (link from page https://www.continuum.io/downloads)
 miniconda="Miniconda3-4.3.14-MacOSX-x86_64.sh" # update if necessary
-wget -P Downloads/ https://repo.continuum.io/miniconda/${miniconda}
+wget -P ~/Downloads/ https://repo.continuum.io/miniconda/${miniconda}
 # install miniconda - accept default options except yes to prepend anaconda path to PATH
 bash ~/Downloads/${miniconda}
 
 # run .bashrc to update path
-. ~/.bashrc
+source ~/.bashrc
 
 # update python packages - using conda - example
 conda update -y conda jupyter jupyter_client jupyter_console jupyter_core \
