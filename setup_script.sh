65eb13c2ec52722ecc7957dbaaa3f37dd958c268
Siddharth Sachdeva <siddsach@gmail.com>
2021-10-28 15:08:23 -0700

    Correct conda location

--- setup_script.sh
+++ setup_script.sh
@@ -7,7 +7,7 @@ sudo apt-get -y install bzip2 wget git unzip
 # MINICONDA
 # download Miniconda linux version (link from page https://www.continuum.io/downloads)
 miniconda="Miniconda3-py39_4.9.2-Linux-x86_64.sh" # update if necessary
-wget -P Downloads/ https://repo.continuum.io/miniconda/${miniconda}
+wget -P ~/Downloads/ https://repo.continuum.io/miniconda/${miniconda}
 # install miniconda - accept default options except yes to prepend anaconda path to PATH
 bash ~/Downloads/${miniconda}
 
